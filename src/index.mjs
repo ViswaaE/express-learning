da8af6047f545917d0538d1bc88e0aa5b6f82031
ViswaaE <viswaaeswaran@gmail.com>
2025-03-06 23:47:08 +0530

    query params

--- src/index.mjs
+++ src/index.mjs
@@ -1,27 +1,49 @@
 import express from "express";
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.get("/",(request,response )=>{
-    response.send({msg:"hello"})
-})
+//user data added
+const mockuser = [
+    { id:1, username: "viswaa", Displayname: "Viswaa" },
+    { id:2, username: "vinson", Displayname: "Vinson" },
+    { id:3, username: "sathish", Displayname: "Sathish" }
+];
 
+app.get("/", (request, response) => {
+    response.status(201).send({ msg: "hello hii" });
+});
+
+
+//display all the user information
 app.get('/api/users', (request, response) => {
+    response.send(mockuser);
+});
+
+
+//for particular selection or for desired user data to watch
+app.get('/api/user/:id', (request, response) => {
+    console.log(request.params);
+    const parsedId = parseInt(request.params.id);
+
+    if (isNaN(parsedId))  
+        return response.status(400).send({ msg: "Invalid Id" });
+
+    const finduser = mockuser.find((user) => user.id === parsedId);
+
+    if (!finduser) 
+        return response.status(404).send({ msg: "User not found" });
+
+    return response.send(finduser);
+});
+
+
+app.get('/api/product', (request, response) => {
     response.send([
-        { id: 1, username: "viswaa", username: "Viswaa" },
-        { id: 1, username: "vinson", username: "Vinson" },
-        { id: 1, username: "samuel", username: "Samuel" }
+        { id: 1, productname: "apple", Displayname: "Apple" }
     ]);
 });
 
-app.get('/api/product',(request,response)=>{
-    response.send([
-        {id:1,}
-    ])
-})
-
 app.listen(PORT, () => {
-    console.log(`Runnig on port ${PORT}`);
+    console.log(`Running on port ${PORT}`);
 });
